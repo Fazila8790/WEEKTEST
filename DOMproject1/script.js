0c685f817cd223615000646f4e3c48582d264920
Fazila8790 <168949893+Fazila8790@users.noreply.github.com>
2024-10-23 19:06:16 +0530

    create two files for dom project1

---
+++ DOMproject1/script.js
@@ -0,0 +1,26 @@
+
+const divElement = document.getElementById('myDiv');
+
+        // Change the background color to yellow
+        divElement.style.backgroundColor = 'yellow';
+
+        // Add a margin of 20 pixels
+        divElement.style.margin = '20px';
+
+        // Apply a padding of 10 pixels
+        divElement.style.padding = '10px';
+
+        // Change the font size to 18 pixels
+        divElement.style.fontSize = '18px';
+
+        // Set the font weight to bold
+        divElement.style.fontWeight = 'bold';
+
+        // Change the height of the element to 200 pixels
+        divElement.style.height = '200px';
+
+        // Modify the width of the element to 300 pixels
+        divElement.style.width = '300px';
+
+        // Change the font color to blue as per UI
+        divElement.style.color = 'blue';
